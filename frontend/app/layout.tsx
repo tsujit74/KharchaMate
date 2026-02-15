8918a7d000454e447dbcf7c826488593dab340cb
Sujit Thakur <tsujeet440@gmail.com>
2026-02-15 11:43:30 +0530

    Layout Added

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,62 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Providers from "./providers";
+import OfflineBanner from "@/app/components/offlineBanner";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "KharchaMate",
+  description: "Smart expense splitting for groups",
+  manifest: "/manifest.json",
+  icons: {
+    icon: "/kms.svg",
+  },
+};
+
+export const viewport = {
+  themeColor: "#000000",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={`
+          ${geistSans.variable}
+          ${geistMono.variable}
+          antialiased
+          bg-[#FCFCFD]
+          text-gray-900
+        `}
+      >
+        <Providers>
+          <OfflineBanner />
+          <Navbar />
+
+          <main className="min-h-screen pt-16">
+            {children}
+          </main>
+
+          <Footer />
+        </Providers>
+      </body>
+    </html>
+  );
+}
